3631c4e6ffd928574db7e81a1032eba0938f2e96
ariburaco <ozdenaliburak@gmail.com>
2022-08-30 21:10:13 +0300

    redux implemented

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,14 +1,26 @@
-import { StyleSheet } from 'react-native';
+import EditScreenInfo from "components/EditScreenInfo";
+import { View, Text } from "components/Themed";
+import { Button, StyleSheet } from "react-native";
+import { useAppDispatch, useAppSelector } from "store/hooks";
+import { decrement, increment, selectCount } from "store/slices/counterSlice";
+import { RootTabScreenProps } from "types";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
-
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  const dispatch = useAppDispatch();
+  const count = useAppSelector(selectCount);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <Button title="Up" onPress={() => dispatch(increment())} />
+      <Text style={styles.title}>Count: {count}</Text>
+      <Button title="Down" onPress={() => dispatch(decrement())} />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
@@ -17,16 +29,16 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
